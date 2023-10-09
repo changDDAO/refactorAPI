df1087ee7dd86d20849f119930949e0f0f562b8f
Changhogod <younch8342@naver.com>
2023-10-09 20:58:50 +0900

    Initial commit

---
+++ src/main/java/com/changddao/refactorAPI/RefactorApiApplication.java
@@ -0,0 +1,13 @@
+package com.changddao.refactorAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/changddao/refactorAPI/RefactorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changddao.refactorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
